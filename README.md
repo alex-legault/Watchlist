45ae09528de747958550277378f26d8918be4d41
Alex Legault <allegault99@gmail.com>
2020-10-07 00:08:42 -0700

    Added window display for 5 placeholder stocks. Next task is to add user input of the 5 stocks

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A simple Stock Watchlist
 
 #Ideas
-gui window with a list of stocks 
-can add or delete tickers
-would have current price, current increase in percent and dollar
-max 500 api requests per day
+gui window with a list of stocks - DONE
+can add or delete tickers - working on adding
+would have current price, current increase in percent and dollar - DONE
+max 500 api requests per day - this is a limitation
--- main.py
+++ main.py
@@ -2,22 +2,93 @@ from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
 from datetime import date
 import json
+import tkinter as tk
+from tkinter import ttk
+
 
 #Use Alpha Vantage API Key to access updated stock market data and metadata
 ts = TimeSeries(key='5ZRUIOQ3NN7GYT1Z')
 
-#convert a ticker's daily information to json object
-msft = ts.get_daily(symbol='MSFT')
-msft_str_obj = json.dumps(msft)
-msft_json_obj = json.loads(msft_str_obj)
+def display_table(tickers, table):
+    stock_list = []
+    #tickers = ['MSFT', 'AAPL', 'TSLA', 'PRPL', 'WM']
+    for ticker in tickers:
+        ticker_json_obj = json.loads(json.dumps(ts.get_daily(symbol=ticker)))
+        ticker_latest_date = ticker_json_obj[1]["3. Last Refreshed"]
 
-#find latest date ticker was closed
-#this implies the watchlist does not track intraday activity
-latest_date = msft_json_obj[1]["3. Last Refreshed"]
+        ticker_info = []
+        ticker_info.append(ticker)
+        ticker_info.append(round(float(ticker_json_obj[0][ticker_latest_date]['1. open']), 2))
+        ticker_info.append(round(float(ticker_json_obj[0][ticker_latest_date]['4. close']), 2))
+        ticker_info.append(round(((float(ticker_json_obj[0][ticker_latest_date]['4. close']) - float(ticker_json_obj[0][ticker_latest_date]['1. open'])) / float(ticker_json_obj[0][ticker_latest_date]['1. open'])) * 100.0, 2))
+        ticker_info.append(ticker_latest_date)
+        stock_list.append(ticker_info)
+    stock_list.sort(key=lambda e: e[1], reverse=True)
 
-print(type(msft_json_obj[0][latest_date]['4. close']))
-print(msft_json_obj[0][latest_date]['4. close'])
+    for stock in stock_list:
+        table.insert("", "end", values=stock)
 
-#print(type(msft))
-#print(type(formatted_msft))
-#print(formatted_msft)
+def show_stocks(tickers):
+    stocks = tk.Tk()
+
+    label = tk.Label(stocks, text="Stock Watchlist", font=("Arial", 24)).grid(row=0, columnspan=5)
+
+    cols = ('Ticker', 'Open', 'Close', 'Up/Down', 'Date')
+    table = ttk.Treeview(stocks, columns=cols, show="headings")
+
+    for col in cols:
+        table.heading(col, text=col)
+
+    table.grid(row=1, column=0, columnspan=2)
+
+    loadStocks = tk.Button(stocks, text="Load Stocks", width=15, command= lambda: display_table(tickers, table)).grid(row=4, column=0)
+    closeButton = tk.Button(stocks, text="Exit", width=15, command=exit).grid(row=4, column=1)
+
+    stocks.mainloop()
+
+def input_tickers():
+
+    def generate_window(input_window, fields):
+        for i in range(5):
+            fields.append("Ticker {}: ".format(i+1))
+
+        for field in fields:
+            row = tk.Frame(input_window)
+            label = tk.Label(row, width=15, text=field, anchor='w')
+            ticker = tk.Entry(row)
+            row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+            label.pack(side=tk.LEFT)
+            ticker.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+            values.append((field, ticker))
+        return values
+
+    def fetch(values):
+        for value in values:
+            field = value[0]
+            tickers.append(value[1].get())
+
+    input_window = tk.Tk()
+    fields = []
+    values = []
+    tickers = []
+
+    values = generate_window(input_window, fields)
+    input_window.bind('<Return>', (lambda event, e=values: fetch(e)))
+    submit_button = tk.Button(input_window, text='Submit', command=(lambda e=values: fetch(e)))
+    submit_button.pack(side=tk.LEFT, padx=5, pady=5)
+    exit_button = tk.Button(input_window, text='Exit', command=input_window.quit)
+    exit_button.pack(side=tk.LEFT, padx=5, pady=5)
+    input_window.mainloop()
+
+    return tickers
+
+def main():
+    #tickers = input_tickers()
+
+    #placeholder input, trying to get input_tickers() to work
+    tickers = ['MSFT', 'AAPL', 'WM', 'DIS', 'TSLA']
+    show_stocks(tickers)
+
+
+if __name__ == "__main__":
+    main()
